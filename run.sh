e77190f9e8cd247c350edf2999ef58989085e224
Aaron Sullivan <aasullivan1618@gmail.com>
2026-06-12 11:34:28 -0700

    Remove python version

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-# Resolve the directory of this script, even if symlinked, to run from any folder
-SCRIPT_DIR=$(cd -- "$(dirname -- "$0")" >/dev/null 2>&1 && pwd)
-
-# Run the python interpreter and script relative to the script directory with passed arguments
-"$SCRIPT_DIR/venv/bin/python" "$SCRIPT_DIR/main.py" "$@"
